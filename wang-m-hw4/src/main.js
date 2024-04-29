e93d9a40a178f7a944a67e0d3f224aecddf9160b
kestrelw <mail4melodie@gmail.com>
2024-04-28 23:00:23 -0400

    4.1 errors but works

--- wang-m-hw4/src/main.js
+++ wang-m-hw4/src/main.js
@@ -1,12 +1,18 @@
 import * as map from "./map.js";
 import * as ajax from "./ajax.js";
+import * as storage from "./storage.js"
+import * as parkView from "./parks-viewer.js"
 
 // I. Variables & constants
 // NB - it's easy to get [longitude,latitude] coordinates with this tool: http://geojson.io/
 const lnglatNYS = [-75.71615970715911, 43.025810763917775];
 const lnglatUSA = [-98.5696, 39.8282];
-let favoriteIds = ["p20","p79","p180","p43"];
+let favoriteIds = [/*"p20","p79","p180","p43"*/];
 let geojson;
+
+let button = document.querySelector("#btn-add");
+let buttonDelete = document.querySelector("#btn-delete");
+let description = document.querySelector("#details-2");
 
 // II. Functions
 const setupUI = () => {
@@ -14,19 +20,19 @@ const setupUI = () => {
 	document.querySelector("#btn1").onclick = () => {
 		map.setZoomLevel(5.2);
 		map.setPitchAndBearing(0,0);
-		map.flyTo(lngLatNYS);
+		map.flyTo(lnglatNYS);
 	};
 	// NYS isometric view
 	document.querySelector("#btn2").onclick = () => {
 		map.setZoomLevel(5.5);
 		map.setPitchAndBearing(45,0);
-		map.flyTo(lngLatNYS);
+		map.flyTo(lnglatNYS);
 	};
 	// World zoom 0
 	document.querySelector("#btn3").onclick = () => {
 		map.setZoomLevel(3);
 		map.setPitchAndBearing(0,0);
-		map.flyTo(lngLatUSA);
+		map.flyTo(lnglatUSA);
 	};
 
 	refreshFavorites();
@@ -34,39 +40,70 @@ const setupUI = () => {
 
 const init = () => {
 	map.initMap(lnglatNYS);
+
+	favoriteIds = storage.readFromLocalStorage("favoriteParkIds");
+	console.log(favoriteIds);
+	if(!Array.isArray(favoriteIds)){
+		favoriteIds = [];
+	}
+
 	ajax.downloadFile("data/parks.geojson", (str) => {
 		geojson = JSON.parse(str);
 		console.log(geojson);
 		map.addMarkersToMap(geojson, showFeatureDetails);
 		setupUI();
 	})
+
 };
 
 const getFeatureById = (id) => {
 	const thing = geojson.features.find(it => it.id == id);///RENAME VARIABLES
-	console.log("id thing: "+ thing);
+	//console.log("id thing: "+ thing);
 	return thing;
 }
 	
 const showFeatureDetails = (id) => {
-	console.log(`showFeatureDetails - id=${id}`);
+	//console.log(`showFeatureDetails - id=${id}`);
 	const feature = getFeatureById(id);
 	console.log(feature);
 	document.querySelector("#details-1").innerHTML = `Info for ${feature.properties.title}`;
-	// document.querySelector("#details-2").innerHTML = 
-	// `<b>Address: </b>${feature.properties.address}
-	// <b>Phone: </b><a href="tel:${feature.properties.phone}">${feature.properties.phone}</a>
-	// <b>Website: </b><a href ="${feature.properties.url}">${feature.properties.url}</a>`;
 	document.querySelector("#details-2").innerHTML = 
 		`<p><b>Address: </b>${feature.properties.address}</p>
 		<p><b>Phone: </b><a href="tel:${feature.properties.phone}">${feature.properties.phone}</a></p>
 		<p><b>Website: </b><a href ="${feature.properties.url}">${feature.properties.url}</a></p>`;
-	//'words words \n words';//ITS NOT WORKINGGG
-	
-	// "<b>Address: </b>"+ feature.properties.address
-	// 												+ "\n<b>Phone: </b><a href=tel:"+feature.properties.phone+">"+feature.properties.phone+"</a>"
-	// 												+ "\n<b>Website: </b><a href ="+feature.properties.url+">"+feature.properties.url+"</a>";;
 	document.querySelector("#details-3").innerHTML = `${feature.properties.description}`;
+
+	document.querySelector(".control").style.display = "block";
+	if(favoriteIds.indexOf(id) != -1){
+		button.disabled = true;
+		buttonDelete.disabled = false;
+
+		buttonDelete.onclick = () => {
+			const deleted = favoriteIds.splice([favoriteIds.indexOf(id)], 1);		
+			storage.writeToLocalStorage("favoriteParkIds",favoriteIds);
+			refreshFavorites();
+			button.disabled = false;
+			buttonDelete.disabled = true;
+			parkView.deleteParkNameData(feature.properties.title, feature.id)
+		};
+	}
+	else{
+		button.disabled = false;
+		buttonDelete.disabled = true;
+
+		button.onclick = () => {
+			console.log("button clicked");
+			favoriteIds.push(id);
+			storage.writeToLocalStorage("favoriteParkIds",favoriteIds);
+			refreshFavorites();
+			button.disabled = true;
+			buttonDelete.disabled = false;
+			parkView.writeParkNameData(feature.properties.title, feature.id);
+
+		};
+		  
+	}
+
 };
 
 const refreshFavorites = () =>  {
@@ -95,5 +132,8 @@ const createFavoriteElement = (id) => {
 	`;
 	return a;
 }
+
+
+
 
 init();
